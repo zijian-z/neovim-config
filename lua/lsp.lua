90c2c44947efb8f115d5491d7c188d1fb40dbf68
zijian-z <zijian-zhang@outlook.com>
2022-05-10 08:14:56 +0800

    js代码补全

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -11,7 +11,7 @@ function M.config()
     local lspconfig = require('lspconfig')
 
     -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-    local servers = { 'gopls', 'pyright'}
+    local servers = {'tsserver', 'gopls', 'pyright'}
     for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         -- on_attach = my_custom_on_attach,
